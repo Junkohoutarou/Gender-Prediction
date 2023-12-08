98ac636afa2f1adb8b048e5d222aa4e836a124cc
Nguyen Phu Hao <126962580+Junkohoutarou@users.noreply.github.com>
2023-12-08 09:15:21 +0700

    Update main.py

--- main.py
+++ main.py
@@ -3,16 +3,16 @@ import pickle as pkl
 import numpy as np
 from PIL import Image
 
-class_list = {'0': 'Negative', '1': 'Neutral', '2': 'Positive'}
-st.title("Sentiment analysis from Vietnamese student's feedback")
+class_list = {'0': 'Female', '1': 'Male'}
+st.title("Gender Prediction Based on Vietnamese Names ")
 
 #image = Image.open('ten.png')
 #st.image(image)
 
-input_ec = open('ec_vsfc.pkl', 'rb')
+input_ec = open('ec_vinames.pkl', 'rb')
 encoder = pkl.load(input_ec)
 
-input_md = open('lrc_vsfc.pkl', 'rb')
+input_md = open('lrc_vinames.pkl', 'rb')
 model = pkl.load(input_md)
 
 st.header('Write a feedback')
