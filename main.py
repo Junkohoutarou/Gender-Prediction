193777585e951a83140aa6191f2ad499a62fea65
Nguyen Phu Hao <126962580+Junkohoutarou@users.noreply.github.com>
2023-12-08 09:11:05 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,54 +1,27 @@
 import streamlit as st
-import pandas as pd
-import io
-import matplotlib.pyplot as plt
-import seaborn as sns
+import pickle as pkl
+import numpy as np
+from PIL import Image
 
-st.title('Data visualization')
-st.header('Upload data file')
-data_file = st.file_uploader('Choose a csv file', type=(['.csv']))
+class_list = {'0': 'Negative', '1': 'Neutral', '2': 'Positive'}
+st.title("Sentiment analysis from Vietnamese student's feedback")
 
-if data_file is not None:
-    df = pd.read_csv(data_file)
+#image = Image.open('ten.png')
+#st.image(image)
 
-    st.header('Show data')
-    st.dataframe(df)
+input_ec = open('ec_vsfc.pkl', 'rb')
+encoder = pkl.load(input_ec)
 
-    st.header('Descriptive statistics')
-    st.table(df.describe())
+input_md = open('lrc_vsfc.pkl', 'rb')
+model = pkl.load(input_md)
 
-    st.header('Show data information')
-    buffer = io.StringIO()
-    df.info(buf=buffer)
-    st.text(buffer.getvalue())
+st.header('Write a feedback')
+txt = st.text_area('', '')
 
-    st.header('Visualize each attribute')
-    for col in df.columns:
-        fig, ax = plt.subplots()
-        ax.hist(df[col], bins=20)  
-        plt.xlabel(col)
-        plt.ylabel('Quantity')
-        st.pyplot(fig)
+if txt != '':
+  if st.button('Predict'):
+    feature_vector = encoder.transform([txt])
+    label = str((model.predict(feature_vector))[0])
 
-    st.header('Show correlation between variables')
-    
-    # Xóa các dòng chứa giá trị NaN trước khi tính toán ma trận tương quan
-    df_corr = df.dropna()
-
-    # Kiểm tra xem DataFrame có đủ dữ liệu để tính toán không
-    if len(df_corr) >= 2 and len(df_corr.columns) >= 2 and df_corr.select_dtypes(include='number').shape[1] >= 2:
-        fig, ax = plt.subplots()
-        sns.heatmap(df_corr.corr(method='pearson'), ax=ax, vmax=1, square=True, annot=True, cmap='Reds')
-        st.write(fig)
-    else:
-        st.warning('Not enough numeric data to calculate correlation matrix.')
-
-    output = st.radio('Choose a dependent variable', df.columns)
-    st.header('Show correlation between variables')
-    for col in list(df.columns):
-        if col != output:
-            fig, ax = plt.subplots()
-            ax.scatter(x=df[col], y=df[output])
-            plt.xlabel(col)
-            plt.ylabel(output)
-            st.pyplot(fig)
+    st.header('Result')
+    st.text(class_list[label])  # Added the missing parenthesis and bracket here
