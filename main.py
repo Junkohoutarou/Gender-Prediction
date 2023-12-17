ff4bec7d99d4d0682609ccd9917cee3ee52a6297
Nguyen Phu Hao <126962580+Junkohoutarou@users.noreply.github.com>
2023-12-17 18:19:11 +0700

    Update main.py

--- main.py
+++ main.py
@@ -4,29 +4,32 @@ import numpy as np
 from PIL import Image
 
 class_list = {'0': 'Female', '1': 'Male'}
+
+# Load CSS
 with open("styles.css") as f:
     custom_css = f.read()
-
 st.markdown(f"<style>{custom_css}</style>", unsafe_allow_html=True)
-st.title("Gender Prediction Based on Vietnamese full Names ")
 
-image = Image.open("vinames.png")
-st.image(image)
-
+# Load model and encoder
 input_ec = open('ec_vinames.pkl', 'rb')
 encoder = pkl.load(input_ec)
 
 input_md = open('lrc_vinames.pkl', 'rb')
 model = pkl.load(input_md)
 
+st.title("Gender Prediction Based on Vietnamese Full Names")
+
+# Load image
+image = Image.open("vinames.png")
+st.image(image)
+
 st.header('Write a Vietnamese full name')
 txt = st.text_area('', '')
 
 if txt != '':
-  if st.button('Predict'):
-    feature_vector = encoder.transform([txt])
-    label = str((model.predict(feature_vector))[0])
+    if st.button('Predict'):
+        feature_vector = encoder.transform([txt])
+        label = str((model.predict(feature_vector))[0])
 
-    st.header('Result')
-    st.text(class_list[label])
-
+        st.header('Result')
+        st.text(class_list[label])
